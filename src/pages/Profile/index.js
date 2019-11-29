95ebaf5eefaf96842981690d762eac59f30b6fd1
macemarques <marcelomarques2007@gmail.com>
2019-11-29 09:00:54 -0300

    CHORE: notifications component updated

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+  const dispatch = useDispatch();
+
+  function handleSubmit(data) {
+    // console.tron.log(data);
+    dispatch(updateProfileRequest(data));
+  }
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu melhor E-mail" />
+
+        <hr />
+
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Sua senha atual"
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Nova senha secreta"
+        />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirme sua senha secreta"
+        />
+
+        <button type="submit"> Atualizar perfil</button>
+      </Form>
+      <button type="submit"> Sair do GoBarber</button>
+    </Container>
+  );
 }
